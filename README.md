fe15d26cf64b78968d258be4f33224cc05e430be
Popsana Noble Barida <popsyjunior@gmail.com>
2025-03-25 18:56:43 +0100

    chore: init repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# next-version.sh
+
+- Clone the repo or copy the script and paste in a new bash file
+- Make sure the script is executable:
+  ```chmod +x next-version.sh```
+- Add it to your ~/.bashrc or ~/.bashprofile depending on your os.
+  Example: `alias nextVersion="$HOME/Code/next-version/next-version.sh"`
+- Start using it to create your next release tag on any project
---
+++ next-version.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Get version type (minor, major, patch) from the first argument
+version_type=$1
+
+# Ensure that the user is on the main branch
+current_branch=$(git branch --show-current)
+if [ "$current_branch" != "main" ]; then
+  echo "You must be on the main branch to create a new version."
+  exit 1
+fi
+
+# Check if the version type is valid
+if [ "$version_type" != "major" ] && [ "$version_type" != "minor" ] && [ "$version_type" != "patch" ]; then
+  echo "Invalid version type. Please use 'major', 'minor', or 'patch'."
+  exit 1
+fi
+
+
+# Get the git tag of the last release
+last_tag=$(git describe --tags --abbrev=0)
+
+# Get the version number of the last release
+last_version=$(echo $last_tag | sed 's/v//')
+
+# Increment the version number
+if [ "$version_type" == "major" ]; then
+  new_version=$(echo $last_version | awk -F. '{$1++; print $1".0.0"}')
+elif [ "$version_type" == "minor" ]; then
+  new_version=$(echo $last_version | awk -F. '{$2++; print $1"."$2".0"}')
+else
+  new_version=$(echo $last_version | awk -F. '{$3++; print $1"."$2"."$3}')
+fi
+
+# Print the new version number
+
+echo "New tag: $new_version"
+
+# Create a new git tag
+git tag -a "v$new_version" -m "Version $new_version"
+
+# Push the new tag to the remote repository
+git push origin "v$new_version"
